7418ca9da8a257b8ffc8934155ca8eb1c3a89102
Thiago Cruz <thiago.machado39@etec.sp.gov.br>
2022-03-22 12:34:43 -0300

    Aula com introdução ao PHP

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+class  Pessoa{
+    public $nome;
+    public $endereco;
+    public $bairro;
+    public $cep;
+
+    public function setNome($nome){
+    $this-> nome = $nome;
+}
+public function getNome(){
+    return $this -> nome;
+ }
+
+ public function setEndereco($endereco){
+     $this ->endereco = $endereco;
+ }
+
+ public function getEndereco(){
+     return $this -> endereco;
+ }
+ 
+ public function setBairro($bairro){
+    $this -> bairro = $bairro;
+}
+
+public function getBairro(){
+    return $this -> bairro;
+}
+
+public function setCep($cep){
+    $this ->cep = $cep;
+}
+
+public function getCep(){
+    return $this -> cep;
+}
+}
+
+$temp = new Pessoa();
+
+$temp -> setNome("Thiago Cruz Machado" );
+echo"<h1>" . $temp -> getNome() ."<br>" . "</h1>";
+
+$temp -> setEndereco("ETEC ZONA LESTE") ;
+echo "<h2>" . $temp -> getEndereco()."<br>" . "</h2>";
+
+$temp -> setBairro("HEHEHEHA") ;
+echo "<h3>" .$temp -> getBairro()."<br>" . "</h3>";
+
+$temp -> setCep("18796513879561") ;
+echo "<h4>". $temp -> getCep()."<br>" . "</h4>";
+
+?>
